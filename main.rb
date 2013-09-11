ba3ad04d60390f6b6248a90d42ebdc4d41e42204
woody1983 <unix1983@gmail.com>
2013-09-11 15:53:30 +0800

    first work

---
+++ main.rb
@@ -0,0 +1,23 @@
+require 'yaml'
+
+class Datasource
+  
+  def initialize()
+    @dw = YAML.load(File.open('info_price.yml')) 
+  end
+
+  def get_cpu_info(computer_id)
+    @dw[computer_id]["cpu_info"]
+  end
+
+  def get_cpu_price(computer_id)
+    @dw[computer_id]["cpu_price"]
+  end
+
+end
+
+#-- Debug --#
+
+ds = Datasource.new
+puts ds.get_cpu_info(10001)
+puts ds.get_cpu_price(10002)
