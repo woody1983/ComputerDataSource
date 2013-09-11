a5b07c9816e9730a0c0bf03bdc969411c75bd1ef
woody1983 <unix1983@gmail.com>
2013-09-11 16:17:56 +0800

    Add README.md

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
-#require File.join(__FILE__, './Datasource')
 require File.dirname(__FILE__) + '/Datasource'
+require File.dirname(__FILE__) + '/Computer'
 
 ds = Datasource.new
 puts ds.get_cpu_info(10001)
