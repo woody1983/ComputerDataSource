18d05d2893967d2f2b8f3a4c3b0dc03751b9d0f7
woody1983 <unix1983@gmail.com>
2013-09-11 16:15:42 +0800

    Add Computer class & modify Datasoure require

--- main.rb
+++ main.rb
@@ -1,23 +1,5 @@
-require 'yaml'
-
-class Datasource
-  
-  def initialize()
-    @dw = YAML.load(File.open('info_price.yml')) 
-  end
-
-  def get_cpu_info(computer_id)
-    @dw[computer_id]["cpu_info"]
-  end
-
-  def get_cpu_price(computer_id)
-    @dw[computer_id]["cpu_price"]
-  end
-
-end
-
-#-- Debug --#
+#require File.join(__FILE__, './Datasource')
+require File.dirname(__FILE__) + '/Datasource'
 
 ds = Datasource.new
 puts ds.get_cpu_info(10001)
-puts ds.get_cpu_price(10002)
