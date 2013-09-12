40582643472cc4e9f963408fe438e989ff692503
woody1983 <unix1983@gmail.com>
2013-09-12 14:42:17 +0800

    Modify Computer just send one argument

--- Computer.rb
+++ Computer.rb
@@ -1,19 +1,26 @@
 class Computer
 
-  def initialize(computer_id,data_source)
-    @pc_id = computer_id
+ # instance_methods.each do |m|
+ #   undef_method m unless m.to_s =~ /^__|method_missing|respond_to?|class|methods|object_id/
+ # end
+
+  def initialize(data_source)
+    #@pc_id = computer_id
     @data_source = data_source
-    data_source.methods.grep(/^get_(.*)_info$/) { Computer.define_component $1 }
   end
 
-  def self.define_component(name)
-    define_method(name) {
-      info = @data_source.send "get_#{name}_info", @pc_id
-      price = @data_source.send "get_#{name}_price", @pc_id
+  def method_missing(name, *args)
+      super if !respond_to?(name)
+      puts "#{name}, args: #{args}"
+      info = @data_source.send "get_#{name}_info", args[0]
+      price = @data_source.send "get_#{name}_price", args[0]
       result = "#{name.capitalize}: #{info}, ($#{price})"
       return result = "* #{result}" if price > 1000
       result
-    }
+  end
+
+  def respond_to?(method)
+    @data_source.respond_to?("get_#{method}_info") || super
   end
 
 end
