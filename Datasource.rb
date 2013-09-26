b2b9b8d005f5f0211dae4b1a1a08eeeb9e40d96b
woody1983 <unix1983@gmail.com>
2013-09-26 15:38:09 +0800

    refactoring Datasource

--- Datasource.rb
+++ Datasource.rb
@@ -3,16 +3,47 @@ require 'yaml'
 class Datasource
   
   def initialize()
-    @dw = YAML.load(File.open('info_price.yml')) 
+    @dw = YAML.load(File.open('info_price.yml'))
   end
 
-  def get_cpu_info(computer_id)
+   
+  method_arr = ["get_cpu_info","get_cpu_price",
+                "get_mouse_info","get_mouse_price",
+                "get_memory_info","get_memory_price"]
+ 
+  method_arr.each do |method_ds|
+      method_name = method_ds.sub("get_","") #cpu_info
+      define_method method_ds.to_sym  do | computer_id |
+          @dw[computer_id]["#{method_name}"]
+      end
+  end
+    
+end
+
+
+#-- Debug --#
+=begin
+ds = Datasource.new
+puts ds.get_cpu_info(10001)
+puts ds.get_cpu_price(10002)
+
+
+  def method_missing(name, *args)
+      method_name = name.split '_'
+      define_method :name do
+        @dw[args.first][method_name.last(2).join('_')]
+      end
+  end
+
+
+  define_method method_name.to_sym  do | computer_id |
     @dw[computer_id]["cpu_info"]
   end
 
-  def get_cpu_price(computer_id)
+  define_method :get_cpu_price do | computer_id |
     @dw[computer_id]["cpu_price"]
   end
+
 
   def get_mouse_info(computer_id)
     @dw[computer_id]["mouse_info"]
@@ -29,12 +60,8 @@ class Datasource
   def get_memory_price(computer_id)
     @dw[computer_id]["memory_price"]
   end
-end
-
-
-#-- Debug --#
-=begin
-ds = Datasource.new
-puts ds.get_cpu_info(10001)
-puts ds.get_cpu_price(10002)
+ 
 =end
+
+
+
